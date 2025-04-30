0800371e6f4bfe3268b9e5fd5ec757a05ae968c2
Olubukun Samuel <samuelolubukun@gmail.com>
2025-04-30 06:59:11 +0100

    Update page.tsx

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -7,7 +7,13 @@ import {
 import { notFound } from "next/navigation";
 import ProfilePageClient from "./ProfilePageClient";
 
-export async function generateMetadata({ params }: { params: { username: string } }) {
+interface PageProps {
+  params: {
+    username: string;
+  };
+}
+
+export async function generateMetadata({ params }: PageProps) {
   const user = await getProfileByUsername(params.username);
   if (!user) return;
 
@@ -17,7 +23,7 @@ export async function generateMetadata({ params }: { params: { username: string 
   };
 }
 
-async function ProfilePageServer({ params }: { params: { username: string } }) {
+async function ProfilePageServer({ params }: PageProps) {
   const user = await getProfileByUsername(params.username);
 
   if (!user) notFound();
